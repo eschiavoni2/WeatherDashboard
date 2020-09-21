1e5bc3b4cfa2e1e58c009f5ca4d5bb5fec7e633f
Erika Zibelnik <e.m.zibelnik@gmail.com>
2020-09-21 19:49:07 -0400

    trying to add weather of current day

--- assets/js/java.js
+++ assets/js/java.js
@@ -1,19 +1,19 @@
 $(document).ready(function () {
-    var cities = [];
-    var citiesDiv = document.getElementById("searched-cities");
-    init();
+    // var cities = [];
+    // var citiesDiv = document.getElementById("searched-cities");
+    // init();
 
-    function init() {
-        var savedCities = JSON.parse(localStorage.getItem("cities"));
+    // function init() {
+    //     var savedCities = JSON.parse(localStorage.getItem("cities"));
 
-        if (savedCities !== null) {
-            cities = savedCities
-        }
-    }
+    //     if (savedCities !== null) {
+    //         cities = savedCities
+    //     }
+    // }
 
-    function storingCities () {
-        localStorage.setItem("cities", JSON.stringify(cities));
-    }
+    // function storingCities () {
+    //     localStorage.setItem("cities", JSON.stringify(cities));
+    // }
 
     $("#weather-btn").on("click", function (event) {
         event.preventDefault();
@@ -32,38 +32,44 @@ $(document).ready(function () {
         recentSrch.attr("class", "recentInput");
         $("#recentSearches").append(recentSrch);
 
-    
-        });
-        $("#recentSearches").on("click", function(event) {
-            event.preventDefault();
-            console.log("click");
-            var recentSrch = $(this).siblings().val();
-            console.log(recentSrch);
-        })
+
+    });
+    $("#recentSearches").on("click", function (event) {
+        event.preventDefault();
+        console.log("click");
+        var cityInput = $(this).siblings();
+        console.log(cityInput);
     })
+})
 
-    function currentWeather(city) {
+function currentWeather(city) {
 
-        $.ajax({
-            method: "GET",
-            url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=e4a0807b709fd21166a9113bc8472380&units=imperial"
-        }).then(function (data) {
-            console.log(data);
-        })
-        // $(".city").html("<h1>" + response.name + " Weather Details</h1>");
-        // $(".wind").text("Wind Speed: " + response.wind.speed);
-        // $(".humidity").text("Humidity: " + response.main.humidity);
-    }
+    $.ajax({
+        method: "GET",
+        url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=e4a0807b709fd21166a9113bc8472380&units=imperial"
+    }).then(function (data) {
+        console.log(data);
+    })
+        .then(function (response) {
+            $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+            $(".wind").text("Wind Speed: " + response.wind.speed);
+            $(".humidity").text("Humidity: " + response.main.humidity);
 
-    function forecast(city) {
+            console.log("Wind Speed: " + response.wind.speed);
+            console.log("Humidity: " + response.main.humidity);
+            console.log("Temperature (F): " + tempF);
+        });
+};
 
-        $.ajax({
-            method: "GET",
-            url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=e4a0807b709fd21166a9113bc8472380&units=imperial"
-        }).then(function (data) {
-            console.log(data);
-        })
-    }
+function forecast(city) {
+
+    $.ajax({
+        method: "GET",
+        url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=e4a0807b709fd21166a9113bc8472380&units=imperial"
+    }).then(function (data) {
+        console.log(data);
+    })
+}
 
     // function uv(city) {
     //     $.ajax({
@@ -73,6 +79,6 @@ $(document).ready(function () {
     //         console.log(uvIndex);
     //     })
     // }
-    
+
 
 // });

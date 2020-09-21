47e08eee3aa8e2d1fb546ec5ba43998123a29442
Erika Zibelnik <e.m.zibelnik@gmail.com>
2020-09-21 16:01:12 -0400

    homework startup

---
+++ assets/js/java.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+
+    $("#weather-btn").on("click", function (event) {
+        event.preventDefault();
+
+        console.log("click");
+
+        var cityInput = $("#weather-input").val().trim();
+        console.log(cityInput);
+        currentWeather(cityInput);
+        forecast(cityInput);
+
+    })
+
+    function currentWeather(city) {
+
+        $.ajax({
+            method: "GET"
+            url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=e4a0807b709fd21166a9113bc8472380&units=imperial"
+        }).then(function (data) {
+            console.log(data);
+        })
+    }
+
+    function forecast(city) {
+
+        $.ajax({
+            method: "GET"
+            url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=e4a0807b709fd21166a9113bc8472380&units=imperial"
+        }).then(function (data) {
+            console.log(data);
+        })
+    } 
+
+})

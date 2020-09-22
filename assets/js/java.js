c76d53107ed3875f66641160aa56ae7a2c8c7daa
Erika Zibelnik <e.m.zibelnik@gmail.com>
2020-09-22 19:04:13 -0400

    fixed, some issues.. need help with others..

--- assets/js/java.js
+++ assets/js/java.js
@@ -6,14 +6,13 @@ $(document).ready(function () {
     displayCities();
 
     function init() {
-        var savedCities = JSON.parse(localStorage.getItem("cities"));
+        var savedCities = JSON.parse(localStorage.getItem("cities")) || [];
+        currentWeather(cities[cities.length -1]);
 
         if (savedCities !== null) {
             cities = savedCities
         }
     }
-
-
 
     function displayCities() {
         $('#recentSearches').html('');
@@ -21,17 +20,9 @@ $(document).ready(function () {
             const city = cities[index];
             $("#recentSearches").append('<li><button>'+city+'</button></li>');
         }
-        // use the cities array to list out all of the cities in the html page
-    }
-
-    // var fruits = ['apple', 'banana', 'pear']
-    // fruits.indexOf("apple") => 0
-    // fruits.indexOf('strawberry') => -1
 
      $("#weather-btn").on("click", function (event) {
         event.preventDefault();
-
-    //     console.log("click");
 
         var cityInput = $("#weather-input").val().trim();
         console.log(cityInput);
@@ -43,10 +34,6 @@ $(document).ready(function () {
 
         localStorage.setItem("cities", JSON.stringify(cities));
         displayCities();
-        // push to the cities array
-        // set the cities value in localStorage to the cities array
-        // call displayCities()
-        
 
 
         currentWeather(cityInput);
@@ -57,7 +44,7 @@ $(document).ready(function () {
     $("#recentSearches").on("click", function (event) {
         var city = event.target.textContent
         currentWeather(city)
-    })
+    })}
 })
 
 function currentWeather(city) {
@@ -77,7 +64,6 @@ function currentWeather(city) {
         $(".humidity").text("Humidity: " + response.main.humidity);
 
 
-
         console.log("Wind Speed: " + response.wind.speed);
         console.log("Humidity: " + response.main.humidity);
         console.log("Temperature (F): " + response.main.temp);
@@ -95,8 +81,7 @@ function forecast(city) {
     }).then(function (forecastData) {
         console.log(forecastData);
        for (let index = 0; index < forecastData.list.length; index+=8) {
-           console.log(forecastData.list[index].main.temp);
-            
+
        }
     })
 }
